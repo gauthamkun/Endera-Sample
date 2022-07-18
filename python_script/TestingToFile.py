2e15367608ab65b1c0d4765937e7d6d6d76fbee4
Gautham Krishna <gautham.krishna05@gmail.com>
2022-07-18 10:32:00 -0400

    working file

--- python_script/TestingToFile.py
+++ python_script/TestingToFile.py
@@ -5,32 +5,26 @@ import pandas as pd
 def funs():
     # Change this to the local path.
     # Has the path to the model
-    nlp = spacy.load("C:/Users/Gkrishna/OneDrive - Endera Systems/Documents/Python Scripts/python_stuff_new")
+    nlp = spacy.load('C:/Users/Gkrishna/Downloads/Trained_model/Trained_model')
     # Give the input file
-    df = pd.read_fwf("C:/Users/Gkrishna/Downloads/TestTxt.txt", header=None, names=['Annotations', 'MISCONDUCT',
-                                                                                    'ALCOHOL', 'VEHICULAR', 'FAMILY',
-                                                                                    'MINOR', 'SEX', 'KIDNAP', 'VIOLENCE',
-                                                                                                              'DRUGS',
-                                                                                    'WEAPON', 'FINANCIAL',
-                                                                                    'THEFT', 'FRAUD', 'STALK',
-                                                                                    'TRESPASS',
-                                                                                    'DRUG', 'BURGLARY', 'COURT OFFENSE'
-        , 'LICENSE', 'POLICE OFFENSE', 'HARASSMENT', 'ACCESSORY', 'ANIMAL', 'VEHICHULAR', 'TRESSPASS', 'TRAFFIC',
-                                                                                    'OBSTRUCTION'
-        , 'ASSAULT'])
+    df = pd.read_fwf("C:/Users/Gkrishna/Downloads/TestTxt.txt", header=None, names=['Annotations', 'MISCONDUCT'
+        , 'ALCOHOL', 'VEHICULAR', 'FAMILY', 'MINOR', 'SEX', 'KIDNAP', 'VIOLENCE', 'DRUGS', 'WEAPON', 'FINANCIAL'
+        , 'THEFT', 'FRAUD', 'STALK', 'TRESPASS', 'DRUG', 'BURGLARY', 'COURT OFFENSE', 'LICENSE', 'POLICE OFFENSE'
+        , 'HARASSMENT', 'ACCESSORY', 'ANIMAL', 'VEHICHULAR', 'TRESSPASS', 'TRAFFIC', 'OBSTRUCTION', 'ASSAULT'])
     data = df['Annotations']
     docs1 = list(nlp.pipe(data))
-    i=0
+    i = 0
     for doc in docs1:
+        # doc.cats gives the confidence value or prediction within range from 0 to 1.
+        # Sum of all the labels prediction will be 1
         article_categories = doc.cats
         for k, v in article_categories.items():
             df.loc[i, [k]] = v
 
-
-        i = i+1
+        i = i + 1
     # Give the output path
     # File automatically gets created
-    df.to_csv('C:/Users/Gkrishna/OneDrive - Endera Systems/Desktop/python_script/Output3.csv', encoding='utf-8')
+    df.to_csv('C:/Users/Gkrishna/OneDrive - Endera Systems/Desktop/python_script/Output4.csv', encoding='utf-8')
     print("Data saved to csv file ")
 
 
